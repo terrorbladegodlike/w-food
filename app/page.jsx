ee8ad23e18972f968dd74957eb8ce7fe149b62f3
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-12-18 21:08:12 +0200

    Adding a "Hero" component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,10 @@
+// Import Components
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <main className="w-full max-w-[1440px] bg-white mx-auto overflow-hidden">
-      homepage
+      <Hero/>
     </main>
   )
 }
